da3fcca4ffadd7fbccc98491323f6441da6c2e7d
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-02 16:49:22 +0100

    trying to fix useEffect

--- src/components/FileNamer.js
+++ src/components/FileNamer.js
@@ -1,26 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function FileNamer() {
   const [name, setName] = useState(``);
   const [alert, setAlert] = useState(false);
 
-  useEffect(() => {
-    const handleWindowClick = () => setAlert(false);
-    if (alert) {
-      window.addEventListener(`click`, handleWindowClick);
-    } else {
-      window.removeEventListener(`click`, handleWindowClick);
-    }
-    return () => window.removeEventListener(`click`, handleWindowClick);
-  }, [alert, setAlert]);
-
   function validate(event) {
-    event.preventDefault();
     if (/\*/.test(name)) {
+      event.preventDefault();
       setAlert(true);
       return;
     }
     setAlert(false);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    setAlert(true);
   }
 
   return (
@@ -41,8 +36,8 @@ export default function FileNamer() {
         <div className="information-wrapper">
           <button
             className="information"
-            type="button"
-            onClick={() => setAlert(true)}
+            onClick={handleClick}
+            onBlur={() => setAlert(false)}
           >
             more information
           </button>
