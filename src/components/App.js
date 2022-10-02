c1d598125819939b37a75977359ac9836f29e6ed
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-02 15:49:25 +0100

    made basic form application using state and effect hooks

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FileNamer from "./FileNamer";
+
+function App() {
+  return (
+    <div className="App">
+      <FileNamer />
+    </div>
+  );
+}
+
+export default App;
